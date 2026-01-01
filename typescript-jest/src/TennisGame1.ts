54e22269899ced4dac3a7f9f4c27f00ee8c39d18
KamalAborida <kamal.o.aborida@gmail.com>
2026-01-01 02:21:22 +0200

    ref-ex generateMoreThan4ScorePhrase

--- typescript-jest/src/TennisGame1.ts
+++ typescript-jest/src/TennisGame1.ts
@@ -24,16 +24,13 @@ export class TennisGame1 implements TennisGame {
     let score: string = "";
     let tempScore: number = 0;
     const isScoresEqual = this.m_score1 === this.m_score2;
+    const isAScoreMoreThan4 = this.m_score1 >= 4 || this.m_score2 >= 4;
     
     if (isScoresEqual) {
       const equalScoreValue = this.m_score1 || this.m_score2 || 0;
       score = this.generateEqualScorePhrase(score, equalScoreValue);
-    } else if (this.m_score1 >= 4 || this.m_score2 >= 4) {
-      const minusResult: number = this.m_score1 - this.m_score2;
-      if (minusResult === 1) score = "Advantage player1";
-      else if (minusResult === -1) score = "Advantage player2";
-      else if (minusResult >= 2) score = "Win for player1";
-      else score = "Win for player2";
+    } else if (isAScoreMoreThan4) {
+      score = this.generateMoreThan4ScorePhrase(score);
     } else {
       for (let i = 1; i < 3; i++) {
         if (i === 1) tempScore = this.m_score1;
@@ -58,6 +55,15 @@ export class TennisGame1 implements TennisGame {
       }
     }
 
+    return score;
+  }
+
+  private generateMoreThan4ScorePhrase(score: string) {
+    const minusResult: number = this.m_score1 - this.m_score2;
+    if (minusResult === 1) score = "Advantage player1";
+    else if (minusResult === -1) score = "Advantage player2";
+    else if (minusResult >= 2) score = "Win for player1";
+    else score = "Win for player2";
     return score;
   }
 
